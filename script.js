3408b1e6e51f713c9bc99c6eae22158ae3b0aaeb
tombackert <backert.github@gmail.com>
2024-04-04 22:08:58 +0200

    illusion grafik update

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // script.js
+currentIndex = 0;
 
 function changeColor() {
-  const colors = ['#07f41f', 'F21BE4', '#e90bd3', 'ff0000', '#0727f4', '#a72608', '#7F0799', '#e03616', '#0582CA', '#EE6352', '#FF6B6B', '#840032', '#FF4A1C', '#E71D36', '#B91372', '#138A36', '#E9D758'];
-  const newColor = colors[Math.floor(Math.random() * colors.length)];
+    const colors = ['#07f41f', '#e90bd3', 'ff0000', '#0727f4', '#a72608', '#7F0799', '#e03616', '#0582CA', '#EE6352', '#FF6B6B', '#840032', '#004fff', '#FF4A1C', '#E71D36', '#B91372', '#138A36', '#E9D758', '#C20114', '#F2BB05', '#D00000', '#0000FF', '#00ff00', '#ff00ff'];
+    currentIndex = (currentIndex + 1) % colors.length;
+    const newColor = colors[currentIndex];
 
   // Wähle das übergeordnete Element mit der Klasse "bg-inverse-landing"
   const container = document.querySelector('.bg-inverse-landing');
