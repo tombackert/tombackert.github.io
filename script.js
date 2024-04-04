2fa2f1294a2a7d3054cbf692173c03a549967a8d
tombackert <backert.github@gmail.com>
2024-04-04 21:45:23 +0200

    new color function for illusion grafic

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 // script.js
 
-window.addEventListener('scroll', function () {
-    var footer = document.querySelector('footer');
-    var navbar = document.getElementById('navbar');
+function changeColor() {
+  const colors = ['#07f41f', 'F21BE4', '#e90bd3', 'ff0000', '#0727f4', '#a72608', '#7F0799', '#e03616', '#0582CA', '#EE6352', '#FF6B6B', '#840032', '#FF4A1C', '#E71D36', '#B91372', '#138A36', '#E9D758'];
+  const newColor = colors[Math.floor(Math.random() * colors.length)];
 
-    // Überprüfen, ob der Footer im Sichtbereich ist
-    var footerPosition = footer.getBoundingClientRect().top;
-    var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
-    var footerVisible = footerPosition < viewportHeight;
+  // Wähle das übergeordnete Element mit der Klasse "bg-inverse-landing"
+  const container = document.querySelector('.bg-inverse-landing');
 
-    if (footerVisible) {
-        navbar.classList.add('hidden'); // Navbar ausblenden
-    } else {
-        navbar.classList.remove('hidden'); // Navbar anzeigen
-    }
-});
+  // Füge dem übergeordneten Element den click-Event-Listener hinzu
+  container.addEventListener('click', function() {
+    // Ändere die Farbe aller Elemente mit der Klasse "bg-primary-landing" innerhalb des Containers
+    const elements = container.querySelectorAll('.bg-primary-landing');
+    elements.forEach(element => {
+      element.style.backgroundColor = newColor;
+    });
+  });
+}
+
+// Rufe die Funktion auf, um den Event Listener zu initialisieren
+changeColor();
