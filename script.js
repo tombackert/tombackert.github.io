5b46705c390243671f1264eeccffc9a3aae791c4
tombackert <backert.github@gmail.com>
2024-11-03 23:24:09 -0800

    new footer

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-currentIndex = 0;
+let currentIndex = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Set the initial icon based on the default theme
+  const iconElement = document.getElementById("icon");
+  iconElement.textContent = document.body.classList.contains("light-mode") ? "🌙" : "☀️";
+});
+
+function toggleTheme() {
+  document.body.classList.toggle("light-mode");
+  
+  const iconElement = document.getElementById("icon");
+  iconElement.textContent = document.body.classList.contains("light-mode") ? "🌙" : "☀️";
+}
 
 function changeColor() {
   const colors = [
-    '#07f41f', '#e90bd3', 'ff0000', '#0727f4', '#a72608', '#7F0799', '#e03616',
+    '#07f41f', '#e90bd3', '#ff0000', '#0727f4', '#a72608', '#7F0799', '#e03616',
     '#0582CA', '#EE6352', '#FF6B6B', '#840032', '#004fff', '#FF4A1C', '#E71D36',
     '#B91372', '#138A36', '#E9D758', '#C20114', '#F2BB05', '#D00000', '#0000FF',
     '#00ff00', '#ff00ff'
@@ -10,18 +23,17 @@ function changeColor() {
   currentIndex = (currentIndex + 1) % colors.length;
   const newColor = colors[currentIndex];
 
-  // Select the parent element with class "bg-inverse-landing"
-  const container = document.querySelector('.bg-inverse-landing');
+  // Set link hover color based on selected color
+  document.documentElement.style.setProperty('--link-hover-color', newColor);
 
-  // Add a click event listener to the parent element
-  container.addEventListener('click', function() {
-    // Change the color of all elements with class "bg-primary-landing" inside the container
-    const elements = container.querySelectorAll('.bg-primary-landing');
-    elements.forEach(element => {
-      element.style.backgroundColor = newColor;
-    });
+  // Update the Illusions graphic colors
+  const container = document.querySelector('.bg-inverse-landing');
+  const elements = container.querySelectorAll('.bg-primary-landing');
+  elements.forEach(element => {
+    element.style.backgroundColor = newColor;
   });
 }
 
-// Call the function to initialize the event listener
+// Initialize event listeners
+toggleTheme();
 changeColor();
